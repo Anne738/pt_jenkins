0a70e866cf48f4d8b7ed7492d43a93909064a42e
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-11-22 11:23:47 +0200

    +

---
+++ prog.py
@@ -0,0 +1,25 @@
+
+
+class Order:
+    def __init__(self, item, quantity):
+        self.item = item
+        self.quantity = quantity
+
+class OrderStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, i, q):
+        order = Order(i, q)
+        if order.quantity > 0:
+            self.stack.append(order)
+        else:
+            return False
+
+    def pop(self):
+        order = self.stack.pop()
+
+    def view(self):
+        print("Список замовлень:")
+        for order in self.stack:
+            print(f"- {order.item} ({order.quantity} одиниць)")
